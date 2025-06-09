45110f5867fc896f4a3ff5c994fb3cb0ec7dac36
deanofwalls <deanofwalls@gmail.com>
2025-06-08 21:56:07 -0400

    Added UserMetricsController

--- src/main/java/com/dean/hypertrophy9000/controller/UserMetricsController.java
+++ src/main/java/com/dean/hypertrophy9000/controller/UserMetricsController.java
@@ -1,6 +1,34 @@
 package com.dean.hypertrophy9000.controller;
 
+import com.dean.hypertrophy9000.model.User;
+import com.dean.hypertrophy9000.service.UserMetricsService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user-metrics/")
 public class UserMetricsController {
 
+    private final UserMetricsService userMetricsService;
 
+    public UserMetricsController(UserMetricsService userMetricsService) {
+        this.userMetricsService = userMetricsService;
+    }
+
+    @PostMapping("/complete-metrics")
+    public ResponseEntity<Map<String, Object>> calculateMetrics(@RequestBody User user) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("BMI", userMetricsService.calculateBMI(user));
+        result.put("TDEE", userMetricsService.calculateTDEE(user));
+        result.put("BodyFat", userMetricsService.calculateBodyFatPercentage(user));
+        result.put("FFMI", userMetricsService.calculateFFMI(user));
+        result.put("Calories", userMetricsService.calculateAdvisedCalories(user));
+        return ResponseEntity.ok(result);
+    }
 }
